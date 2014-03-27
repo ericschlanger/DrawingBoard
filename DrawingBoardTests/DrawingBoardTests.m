ecd8204131f8d503acf8228cf436e3d16b27c2cd
Eric Schlanger <ebschlanger@gmail.com>
2014-03-27 10:41:28 -0400

    initial commit

---
+++ DrawingBoardTests/DrawingBoardTests.m
@@ -0,0 +1,34 @@
+//
+//  DrawingBoardTests.m
+//  DrawingBoardTests
+//
+//  Created by Eric Schlanger on 3/27/14.
+//  Copyright (c) 2014 Eric Schlanger. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DrawingBoardTests : XCTestCase
+
+@end
+
+@implementation DrawingBoardTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
